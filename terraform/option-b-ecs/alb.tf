36fd2b6235fbe370340ad6d638a676001ede9476
MarioQuimetMed <mario.medina@ficct.uagrm.edu.bo>
2026-07-06 18:41:48 -0400

    service inventories load balancer

--- terraform/option-b-ecs/alb.tf
+++ terraform/option-b-ecs/alb.tf
@@ -74,21 +74,21 @@ resource "aws_lb_listener_rule" "reservations" {
 # }
 
 
-# resource "aws_lb_target_group" "inventories" {
-#   name        = "${var.project_name}-inventories-tg"
-#   port        = 3000
-#   protocol    = "HTTP"
-#   target_type = "ip"
-#   vpc_id      = aws_vpc.main.id
-# 
-#   health_check {
-#     path                = "/inventories/status/healthcheck"
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 3
-#     interval            = 15
-#     timeout             = 5
-#     matcher             = "200-404" # NestJS devuelve 404 si la ruta no existe, lo que indicará que el server está vivo
-#   }
-# 
-#   deregistration_delay = 10
-# }
+resource "aws_lb_target_group" "inventories" {
+  name        = "${var.project_name}-inventories-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path                = "/inventories/status/healthcheck"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    interval            = 15
+    timeout             = 5
+    matcher             = "200-404" # NestJS devuelve 404 si la ruta no existe, lo que indicará que el server está vivo
+  }
+
+  deregistration_delay = 10
+}
